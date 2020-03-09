0dac03951457ad45d1a91623e68b8b9638f52a62
Vrim <CarbonAmericiumFluorine@gmail.com>
2020-03-09 15:36:10 -0400

    Updates

---
+++ precache-manifest.46b1e474a248b06ef6c7b51e142c2e5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42bdbe4f94f222107f93049a74f7c333",
+    "url": "/calculator-react/index.html"
+  },
+  {
+    "revision": "368d8331e47a49d45dd5",
+    "url": "/calculator-react/static/css/main.59939b0e.chunk.css"
+  },
+  {
+    "revision": "946aa7c3a19ca95d7404",
+    "url": "/calculator-react/static/js/2.bc80099d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/calculator-react/static/js/2.bc80099d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "368d8331e47a49d45dd5",
+    "url": "/calculator-react/static/js/main.7631576c.chunk.js"
+  },
+  {
+    "revision": "a9cbea0bbe5a65b59e75",
+    "url": "/calculator-react/static/js/runtime-main.71e09eff.js"
+  }
+]);
